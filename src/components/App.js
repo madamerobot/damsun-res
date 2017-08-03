cb6541bca22edcdc8525c93e01bb7c0f21241a96
Valerie Fuchs <hi@valerie-fuchs.de>
2017-08-03 16:53:13 +0200

    Remove the now ignored directory node_modules

---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+//CUSTOM COMPONENTS
+import './style.css';
+import Result from './Result';
+import Knob from './Knob';
+import DATA from '../sunspots'
+
+class App extends Component {
+
+  constructor(props){   
+    super(props);
+
+    this.state = {tourist: '4', 
+                  price: '4', 
+                  proof: '4'
+                };
+
+    this.changeTourist = this.changeTourist.bind(this);
+    this.changePrice = this.changePrice.bind(this);
+    this.changeProof = this.changeProof.bind(this);
+  }
+
+  changeTourist (value) {
+    this.setState({ tourist: value })
+  }
+
+  changePrice (value) {
+    this.setState({ price: value })
+  }
+
+  changeProof (value) {
+    this.setState({ proof: value })
+  }
+  
+  render() {
+    return (
+      <div className="machinewrapper">
+
+        <div className="machinearea">
+          
+          <div className="resultpaper">
+            <div className="results">
+              <Result data={DATA} tourist={this.state.tourist} price={this.state.price} proof={this.state.proof} />
+            </div>
+          </div>
+
+          <div className="machine">
+              <div className="Knobbox">
+                <Knob name="Rainproof" value={this.state.proof} onChange={this.changeProof}/>
+                <Knob name="Touristzone" value={this.state.tourist} onChange={this.changeTourist}/>
+                <Knob name="Pricelevel" value={this.state.price} onChange={this.changePrice}/>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/result.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Result extends Component {	
+	
+	render() {
+		
+		const findResults = this._findResults();
+
+		return (
+			<div>
+				{findResults}
+			</div>
+		);
+	}
+
+	_findResults() {
+
+	//FETCHING ALL PROPS I NEED
+	const { data, price, tourist, proof } = this.props;
+	
+	//EMPTY ARRAY FOR RESULTS
+	const results = [];
+
+		data.map((data) => {
+			if(data.Price <= price && data.Touristfactor <= tourist && data.Weatherproof <= proof){				
+				results.push(data.Place);
+				console.log('Results: '+results);
+			}
+		});
+		if(results.length<=0){
+			return <h1>Sorry, you are too picky. No results found.</h1>
+		}
+		return results.map ((results) => {
+			return (
+				<h1 key={results}>{results}📍</h1>
+			)
+		});
+	}
+}
+
+
+export default Result;
