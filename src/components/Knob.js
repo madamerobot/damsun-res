bf1d9f0ad55328c5eba80739a0e4b5dd986afb09
Valerie Fuchs <hi@valerie-fuchs.de>
2017-08-04 12:16:32 +0200

    Moving logic from Knob into Knobbox

--- src/components/Knob.js
+++ src/components/Knob.js
@@ -1,102 +1,8 @@
 import React, { Component } from 'react';
 
 class Knob extends Component {
-
-	constructor(){
-		super();
-		this.state = {
-			x: 296,
-			y: 69,
-			min: 0,
-			max: 5,
-			radius: 125,
-			size: 100,
-			fullAngle: 360,
-			step: 1,
-			rotation: 40
-		} 
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	//HELPER FUNCTIONS
-	_coordToDeg(x,y) {
 	
-		const center = this.state.size / 2;
-		const dX = x - center;
-		const dY = y - center;
-		const teta = Math.atan(dY / dX) * 180 / Math.PI + 90
-		console.log('Teta: '+teta);
-		  
-		if ((dX < 0 && dY >= 0) || (dX < 0 && dY < 0)) {
-		  return teta
-		} else {
-		  return teta + 180
-		}
-	}
-
-	/**
-   * helper that returns the range
-   * @return {Number} - the difference between `min` and `max`
-   */
-	get range() {
-	return this.state.max - this.state.min
-	}
-
-	/**
-   * helper that returns the steps
-   * @return {Number} returns the number of steps from `min` to `max`
-   */
-	get steps() {
-	return Math.round(this.state.range / this.state.step)
-	}
-
-	/**
-   * returns the rest angle which is half of the difference between
-   * `fullAngle` and 360
-   * @return {Number} [description]
-   */
-	get rest() {
-	return (360 - this.state.fullAngle) / 2
-	}
-
-	updateValue(event) {
-	
-		const deg = this._coordToDeg(event.clientX, event.clientY);
-		
-		// console.log('01 Deg: '+deg);
-		// console.log('this.state.fullAngle: '+this.state.fullAngle);
-		// console.log('deg: '+deg);
-		// console.log('this.rest: '+this.rest);
-		
-		const boundDeg = Math.max(0, Math.min(this.state.fullAngle, deg - this.rest));
-		// console.log('02 boundDeg: '+boundDeg);
-		
-		const rawValue = (boundDeg / this.state.fullAngle * this.range + this.state.min)
-		// console.log('03 rawValue: '+rawValue);
-		
-		const value = Math.round(rawValue / this.state.step) * this.state.step;
-		// console.log('04 Value: '+value);
-
-		this.setState({rotation: deg})
-	}
-
-	_onMouseMove(event){		
-		this.updateValue(event);
-	}
-
-	handleChange(event){
-		const value = event.target.value;
-		this.props.onChange(value);
-	}
-
 	render(){
-		
-		const {min, max, step, size, fullAngle} = this.state
-    	const baseAngle = fullAngle / this.range
-
-    	//As soon as handleChange is being triggered, this.props.value changes and thus indicator moves.
-    	//handleChange is hooked up with slider input value (see line 107)
-    	const rotation = this.rest + (this.props.value - min) * baseAngle
 
 		return (
 				<div className="svg">
@@ -113,7 +19,7 @@ class Knob extends Component {
 									strokeWidth="3" 
 									stroke="#848372" 
 									fill="black" 
-									onMouseMove={this._onMouseMove.bind(this)}/>
+									/>
 						 </g>
 						 <g display="inline">
 						  <title>Smaller Circle plus Indicator</title>
@@ -129,7 +35,7 @@ class Knob extends Component {
 						  			stroke="white" 
 						  			fill="none"
 						  			//102,97 defines x/y of the indicator's center point
-						  			transform={`rotate(${rotation},102,97)`}
+						  			transform={`rotate(${this.props.rotation},102,97)`}
 						  	/>
 						  <circle 	id="svg_6" 
 						  			stroke="#f7f714" 
--- src/components/Knobbox.js
+++ src/components/Knobbox.js
@@ -6,16 +6,100 @@ import Result from './Result';
 import Knob from './Knob';
 import DATA from '../sunspots'
 
-
-
 class Knobbox extends Component {
 
+	constructor(){
+		super();
+		this.state = {
+			x: 296,
+			y: 69,
+			min: 0,
+			max: 5,
+			radius: 125,
+			size: 100,
+			fullAngle: 360,
+			step: 1,
+			rotation: 40
+		} 
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	//HELPER FUNCTIONS
+	_coordToDeg(x,y) {
+	
+		const center = this.state.size / 2;
+		const dX = x - center;
+		const dY = y - center;
+		const teta = Math.atan(dY / dX) * 180 / Math.PI + 90
+		console.log('Teta: '+teta);
+		  
+		if ((dX < 0 && dY >= 0) || (dX < 0 && dY < 0)) {
+		  return teta
+		} else {
+		  return teta + 180
+		}
+	}
+
+	/**
+   * helper that returns the range
+   * @return {Number} - the difference between `min` and `max`
+   */
+	get range() {
+	return this.state.max - this.state.min
+	}
+
+	/**
+   * helper that returns the steps
+   * @return {Number} returns the number of steps from `min` to `max`
+   */
+	get steps() {
+	return Math.round(this.state.range / this.state.step)
+	}
+
+	/**
+   * returns the rest angle which is half of the difference between
+   * `fullAngle` and 360
+   * @return {Number} [description]
+   */
+	get rest() {
+	return (360 - this.state.fullAngle) / 2
+	}
+
+	updateValue(event) {
+	
+		const deg = this._coordToDeg(event.clientX, event.clientY);
+				
+		const boundDeg = Math.max(0, Math.min(this.state.fullAngle, deg - this.rest));
+		
+		const rawValue = (boundDeg / this.state.fullAngle * this.range + this.state.min)
+		
+		const value = Math.round(rawValue / this.state.step) * this.state.step;
+
+		this.setState({rotation: deg})
+	}
+
+	_onMouseMove(event){		
+		this.updateValue(event);
+	}
+
+	handleChange(event){
+		const value = event.target.value;
+		this.props.onChange(value);
+	}
+
 	render() {
+		const {min, max, step, size, fullAngle} = this.state
+    	const baseAngle = fullAngle / this.range
+
+    	//As soon as handleChange is being triggered, this.props.value changes and thus indicator moves.
+    	//handleChange is hooked up with slider input value (see line 107)
+    	const rotation = this.rest + (this.props.value - min) * baseAngle
+
 		return (
 			
 			<div className="Knobbox">
 								
-				<Knob name={this.props.name} value={this.props.value} onChange={this.props.onChange}/>
+				<Knob name={this.props.name} value={this.props.value} onChange={this.props.onChange} rotation={this.state.rotation}/>
 				
 				<div className="label">Rainproof</div>
 				
